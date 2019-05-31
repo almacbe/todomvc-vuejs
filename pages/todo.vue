26ac25572f4c910294db8d548669d4faef4e2a17
Alfonso Machado <amachado@wearemarketing.com>
2019-05-31 21:20:28 +0200

    create todo page

---
+++ pages/todo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <todo />
+    <div class="info">
+      <p>Double-click to edit a todo</p>
+      <!-- Remove the below line ↓ -->
+      <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+      <!-- Change this out with your name and url ↓ -->
+      <p>Created by <a href="http://todomvc.com">you</a></p>
+      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from '~/components/Todo.vue'
+
+export default {
+  components: {
+    Todo
+  },
+  head() {
+    return {
+      title: 'TodoMVC'
+    }
+  }
+}
+</script>
