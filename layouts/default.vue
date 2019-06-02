b13e81043e81c464b22b2296035218073fd9a9dd
Alfonso Machado <amachado@wearemarketing.com>
2019-06-03 00:23:53 +0200

    fix link

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <nuxt />
     <div class="info">
       <p>Double-click to edit a todo</p>
-      <p>Created by <a href="https://alfonsomachado.com">Alfonso Machado</a></p>
+      <p>Created by <a href="http://alfonsomachado.com">Alfonso Machado</a></p>
       <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
     </div>
   </div>
